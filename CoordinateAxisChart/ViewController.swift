3ae074710fd1010b861644b7bfe2b29aa992fb5a
CrystalMarch <zhuhuiping@shinetechchina.com>
2018-02-09 16:38:36 +0800

    add gif demo

--- CoordinateAxisChart/ViewController.swift
+++ CoordinateAxisChart/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         refreshButton.setTitle("refresh", for: .normal)
         refreshButton.addTarget(self, action: #selector(self.refreshButonClick(sender:)), for: .touchUpInside)
         refreshButton.setTitleColor(.gray, for: .normal)
+        refreshButton.setTitleColor(.red, for: .highlighted)
         refreshButton.layer.borderWidth = 1
         refreshButton.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(refreshButton)
@@ -27,6 +28,7 @@ class ViewController: UIViewController {
         clearButton.setTitle("clear", for: .normal)
         clearButton.addTarget(self, action: #selector(self.clearButtonClick(sender:)), for: .touchUpInside)
         clearButton.setTitleColor(.gray, for: .normal)
+        clearButton.setTitleColor(.red, for: .highlighted)
         clearButton.layer.borderWidth = 1
         clearButton.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(clearButton)
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
         changeAxisValueButton.setTitle("change coordinate axis value", for: .normal)
         changeAxisValueButton.addTarget(self, action: #selector(self.changeAxisValueButtonClick(sender:)), for: .touchUpInside)
         changeAxisValueButton.setTitleColor(.gray, for: .normal)
+        changeAxisValueButton.setTitleColor(.red, for: .highlighted)
         changeAxisValueButton.layer.borderWidth = 1
         changeAxisValueButton.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(changeAxisValueButton)
@@ -44,6 +47,7 @@ class ViewController: UIViewController {
         changeAxisColorButton.setTitle("change coordinate axis color", for: .normal)
         changeAxisColorButton.addTarget(self, action: #selector(self.changeAxisColorButtonClick(sender:)), for: .touchUpInside)
         changeAxisColorButton.setTitleColor(.gray, for: .normal)
+        changeAxisColorButton.setTitleColor(.red, for: .highlighted)
         changeAxisColorButton.layer.borderWidth = 1
         changeAxisColorButton.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(changeAxisColorButton)
