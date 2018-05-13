c0771669acaa432752df7640ea8b1d5da4d337fd
Adam Nemecek <adamnemecek@gmail.com>
2018-05-13 11:28:07 -0700

    Refactored buttons into classes.

--- CoordinateAxisChart/ViewController.swift
+++ CoordinateAxisChart/ViewController.swift
@@ -10,46 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
     let chartView = CoordinateAxisChart()
+
+    final class RefreshButton: UIButton {
+        convenience init(viewController: ViewController) {
+            self.init()
+            frame = CGRect(x:30,y:40,width:120,height:30)
+            setTitle("refresh", for: .normal)
+            addTarget(self, action: #selector(viewController.refreshButonClick), for: .touchUpInside)
+            setTitleColor(.gray, for: .normal)
+            setTitleColor(.red, for: .highlighted)
+            layer.borderWidth = 1
+            layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+
+    final class ClearButton: UIButton {
+        convenience init(viewController: ViewController) {
+            self.init()
+            frame = CGRect(x: viewController.view.frame.size.width-150, y: 40, width: 120, height: 30)
+            setTitle("clear", for: .normal)
+            addTarget(self, action: #selector(viewController.clearButtonClick), for: .touchUpInside)
+            setTitleColor(.gray, for: .normal)
+            setTitleColor(.red, for: .highlighted)
+            layer.borderWidth = 1
+            layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+
+    final class ChangeAxisValueButton: UIButton {
+        convenience init(viewController: ViewController) {
+            self.init()
+            frame = CGRect(x:30,y:80,width: viewController.view.frame.size.width - 60,height:30)
+            setTitle("change coordinate axis value", for: .normal)
+            addTarget(self, action: #selector(viewController.changeAxisValueButtonClick), for: .touchUpInside)
+            setTitleColor(.gray, for: .normal)
+            setTitleColor(.red, for: .highlighted)
+            layer.borderWidth = 1
+            layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+
+    final class ChangeAxisColorButton: UIButton {
+        convenience init(viewController: ViewController) {
+            self.init()
+            frame = CGRect(x:30,y:120,width: viewController.view.frame.size.width - 60,height:30)
+            setTitle("change coordinate axis color", for: .normal)
+            addTarget(self, action: #selector(viewController.changeAxisColorButtonClick), for: .touchUpInside)
+            setTitleColor(.gray, for: .normal)
+            setTitleColor(.red, for: .highlighted)
+            layer.borderWidth = 1
+            layer.borderColor = UIColor.gray.cgColor
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let refreshButton = UIButton()
-        refreshButton.frame = CGRect(x:30,y:40,width:120,height:30)
-        refreshButton.setTitle("refresh", for: .normal)
-        refreshButton.addTarget(self, action: #selector(self.refreshButonClick(sender:)), for: .touchUpInside)
-        refreshButton.setTitleColor(.gray, for: .normal)
-        refreshButton.setTitleColor(.red, for: .highlighted)
-        refreshButton.layer.borderWidth = 1
-        refreshButton.layer.borderColor = UIColor.gray.cgColor
+        let refreshButton = RefreshButton(viewController: self)
         self.view.addSubview(refreshButton)
-        let clearButton =  UIButton()
-        clearButton.frame = CGRect(x:self.view.frame.size.width-150,y:40,width:120,height:30)
-        clearButton.setTitle("clear", for: .normal)
-        clearButton.addTarget(self, action: #selector(self.clearButtonClick(sender:)), for: .touchUpInside)
-        clearButton.setTitleColor(.gray, for: .normal)
-        clearButton.setTitleColor(.red, for: .highlighted)
-        clearButton.layer.borderWidth = 1
-        clearButton.layer.borderColor = UIColor.gray.cgColor
+
+        let clearButton = ClearButton(viewController: self)
         self.view.addSubview(clearButton)
-        let changeAxisValueButton =  UIButton()
-        changeAxisValueButton.frame = CGRect(x:30,y:80,width:self.view.frame.size.width - 60,height:30)
-        changeAxisValueButton.setTitle("change coordinate axis value", for: .normal)
-        changeAxisValueButton.addTarget(self, action: #selector(self.changeAxisValueButtonClick(sender:)), for: .touchUpInside)
-        changeAxisValueButton.setTitleColor(.gray, for: .normal)
-        changeAxisValueButton.setTitleColor(.red, for: .highlighted)
-        changeAxisValueButton.layer.borderWidth = 1
-        changeAxisValueButton.layer.borderColor = UIColor.gray.cgColor
+
+        let changeAxisValueButton = ChangeAxisValueButton(viewController: self)
         self.view.addSubview(changeAxisValueButton)
 
-        let changeAxisColorButton =  UIButton()
-        changeAxisColorButton.frame = CGRect(x:30,y:120,width:self.view.frame.size.width - 60,height:30)
-        changeAxisColorButton.setTitle("change coordinate axis color", for: .normal)
-        changeAxisColorButton.addTarget(self, action: #selector(self.changeAxisColorButtonClick(sender:)), for: .touchUpInside)
-        changeAxisColorButton.setTitleColor(.gray, for: .normal)
-        changeAxisColorButton.setTitleColor(.red, for: .highlighted)
-        changeAxisColorButton.layer.borderWidth = 1
-        changeAxisColorButton.layer.borderColor = UIColor.gray.cgColor
+        let changeAxisColorButton = ChangeAxisColorButton(viewController: self)
         self.view.addSubview(changeAxisColorButton)
 
         chartView.frame = CGRect(x:50,y:170,width:220,height:220)
@@ -79,12 +105,15 @@ class ViewController: UIViewController {
     func refreshButonClick(sender:UIButton) {
         chartView.refresh()
     }
+
     func clearButtonClick(sender:UIButton) {
         chartView.clear()
     }
+
     func changeAxisColorButtonClick(sender: UIButton) {
         chartView.axisColor = .random()
     }
+
     func changeAxisValueButtonClick(sender:UIButton) {
         chartView.xMaxValue = Int(arc4random()%10)
         chartView.xMinValue = -Int(arc4random()%10)
